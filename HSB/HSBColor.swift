b04e312f4dcbffaab4c7d4a80d6d9f895ce27edd
durul <adurul17@hotmail.com>
2016-02-13 14:51:13 -0500

    "Completed HSB iOS framework!"

---
+++ HSB/HSBColor.swift
@@ -0,0 +1,22 @@
+//
+//  HSBColor.swift
+//  HSB
+//
+//  Created by durul dalkanat on 2/13/16.
+//  Copyright © 2016 durul dalkanat. All rights reserved.
+//
+
+import Curry
+
+public func HSBColor(hue: Int, saturation: Double, brightness: Double) -> UIColor {
+    return curry(createColor)(hue)(saturation)(brightness)
+}
+
+private func createColor(hue: Int, saturation: Double, brightness: Double) -> UIColor {
+    return UIColor(
+        hue: CGFloat(1),
+        saturation: CGFloat(1),
+        brightness: CGFloat(1),
+        alpha: 1
+    )
+}
---
+++ HSBTests/HSBTests.swift
@@ -0,0 +1,30 @@
+//
+//  HSBTests.swift
+//  HSBTests
+//
+//  Created by durul dalkanat on 2/13/16.
+//  Copyright © 2016 durul dalkanat. All rights reserved.
+//
+
+import Quick
+import Nimble
+import HSB
+
+class RGBTests: QuickSpec {
+    override func spec() {
+        describe("HSBUIColor") {
+            it("is a correct representation of the values") {
+                let hsbPing = UIColor(
+                    hue: CGFloat(1),
+                    saturation: CGFloat(1),
+                    brightness: CGFloat(1),
+                    alpha: 1
+                )
+                
+                let color = HSBColor(336, saturation: 68.05, brightness: 66.27)
+                
+                expect(color).to(equal(hsbPing))
+            }
+        }
+    }
+}
